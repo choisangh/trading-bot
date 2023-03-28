788db76083f6376fe7191fe5bc1e00bd1c0bc1b3
SH.CHOI <92343445+choisangh@users.noreply.github.com>
2023-03-28 23:57:46 +0900

    feat: dash 업데이트

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import dash
-from dash import dash_table
+from dash.dependencies import Input, Output
+from dash import dash_table, html, dcc
 import pandas as pd
 import sqlite3
-
 # SQLite DB 연결
 conn = sqlite3.connect('returns.db')
 
@@ -21,8 +21,36 @@ table = dash_table.DataTable(
     data=df.to_dict('records'),
 )
 
+
+
+
 # 애플리케이션 레이아웃 설정
-app.layout = table
+app.layout = html.Div([
+    dcc.Interval(
+        id='interval-component',
+        interval=10 * 1000,  # 10초마다 업데이트
+        n_intervals=0
+    ),
+    dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data=[],
+    ),
+])
+
+
+def update_data():
+    conn = sqlite3.connect('returns.db')
+    df = pd.read_sql_query("SELECT * from trades", conn)
+    conn.close()
+    return df.to_dict('records')
+
+@app.callback(
+    Output('table', 'data'),
+    [Input('interval-component', 'n_intervals')])
+def update_table(n):
+    return update_data()
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8080)
